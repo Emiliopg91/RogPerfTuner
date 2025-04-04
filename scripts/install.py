da96a39c8d6c5da6f9cf6ad91d8f79bfd5634b1e
Emiliopg91 <ojosdeserbio@gmail.com>
2025-04-04 21:00:54 +0200

    Scripting

--- scripts/install.py
+++ scripts/install.py
@@ -17,55 +17,53 @@ def install_uv_deps():
     subprocess.run(command, shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
 
 
+def get_last_mod_time(directorio):
+    ultima_fecha = None
+
+    for carpeta_raiz, _, archivos in os.walk(directorio):
+        for archivo in archivos:
+            ruta_completa = os.path.join(carpeta_raiz, archivo)
+            try:
+                timestamp = os.path.getmtime(ruta_completa)
+                if ultima_fecha is None or timestamp > ultima_fecha:
+                    ultima_fecha = timestamp
+            except Exception:
+                pass
+    return ultima_fecha
+
+
 def get_openrgb():
     """Download latest OpenRGB"""
-    print("  Installing OpenRGB...")
-    import requests
+    print("  Building OpenRGB...")
 
-    url = "https://api.github.com/repos/Emiliopg91/OpenRGB/releases/latest"
-    response = requests.get(url)
+    if not os.path.exists(OPENRGB_PATH) or os.path.getmtime(OPENRGB_PATH) < get_last_mod_time(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "submodules", "OpenRGB"))
+    ):
+        if os.path.exists(OPENRGB_PATH):
+            os.unlink(OPENRGB_PATH)
+        subprocess.run(
+            "./build.sh",
+            cwd=os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "submodules", "OpenRGB")),
+            check=True,
+            shell=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+        )
+        shutil.copy2("./submodules/OpenRGB/OpenRGB-Exp.AppImage", OPENRGB_PATH)
+        os.chmod(OPENRGB_PATH, 0o755)
 
-    file_mod_date = (
-        datetime.datetime.fromtimestamp(os.path.getmtime(OPENRGB_PATH)) if os.path.exists(OPENRGB_PATH) else None
-    )
+        print("    Built, getting udev file...")
 
-    if response.status_code == 200:
-        release_data = response.json()
-        data = [asset for asset in release_data["assets"] if asset["name"].endswith(".AppImage")]
-        if len(data) > 0:
-            asset = data[0]
-            if file_mod_date is None or file_mod_date < datetime.datetime.strptime(
-                asset["created_at"], "%Y-%m-%dT%H:%M:%SZ"
-            ):
-                print("    Downloading dependency...")
-                url = asset["browser_download_url"]
+        os.chdir(os.path.dirname(OPENRGB_PATH))
+        command = " ".join([OPENRGB_PATH, "--appimage-extract"])
+        subprocess.run(command, shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+        squashfs_root = os.path.join(os.path.dirname(OPENRGB_PATH), "squashfs-root")
 
-                response = requests.get(url, stream=True)
-                response.raise_for_status()
+        src_udev_path = os.path.join(squashfs_root, "usr", "lib", "udev", "rules.d", "60-openrgb.rules")
+        shutil.copy2(src_udev_path, UDEV_PATH)
 
-                with open(OPENRGB_PATH, "wb") as f:
-                    for chunk in response.iter_content(chunk_size=8192):
-                        f.write(chunk)
-                os.chmod(OPENRGB_PATH, 0o755)
-
-                print("    Dependency updated, getting udev file...")
-
-                os.chdir(os.path.dirname(OPENRGB_PATH))
-                command = " ".join([OPENRGB_PATH, "--appimage-extract"])
-                subprocess.run(command, shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
-                squashfs_root = os.path.join(os.path.dirname(OPENRGB_PATH), "squashfs-root")
-
-                src_udev_path = os.path.join(squashfs_root, "usr", "lib", "udev", "rules.d", "60-openrgb.rules")
-                shutil.copy2(src_udev_path, UDEV_PATH)
-
-                shutil.rmtree(squashfs_root)
-                print("    Udev file extracted")
-            else:
-                print("    Dependency up to date")
-        else:
-            raise FileNotFoundError("No URL published for OpenRGB")
-    else:
-        raise Exception(f"Error getting latest release: {response.status_code}")
+        shutil.rmtree(squashfs_root)
+        print("    Udev file extracted")
 
 
 def get_rccdc():
@@ -105,6 +103,7 @@ def get_rccdc():
                 subprocess.run(
                     command,
                     check=True,
+                    shell=True,
                     stdout=subprocess.DEVNULL,
                     stderr=subprocess.PIPE,
                 )
