bc25a6c6fedb711dfb987474cc04c664d2a3e3ee
Emiliopg91 <ojosdeserbio@gmail.com>
2025-04-04 21:29:28 +0200

    RCCDeckyCompanion submodule

--- scripts/install.py
+++ scripts/install.py
@@ -68,53 +68,30 @@ def get_openrgb():
 
 def get_rccdc():
     """Download latest RCCDeckyCompanion"""
-    print("  Installing RCCDeckyCompanion...")
-    import requests
+    print("  Building RCCDeckyCompanion...")
 
-    url = "https://api.github.com/repos/Emiliopg91/RCCDeckyCompanion/releases/latest"
-    response = requests.get(url)
+    if not os.path.exists(RCCDC_PATH) or os.path.getmtime(RCCDC_PATH) < get_last_mod_time(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "submodules", "RCCDeckyCompanion"))
+    ):
+        if os.path.exists(RCCDC_PATH):
+            shutil.rmtree(RCCDC_PATH)
+        subprocess.run(
+            "./cli/decky.py build",
+            cwd=os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "submodules", "RCCDeckyCompanion")),
+            check=True,
+            shell=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+        )
 
-    file_mod_date = (
-        datetime.datetime.fromtimestamp(os.path.getmtime(RCCDC_PATH)) if os.path.exists(RCCDC_PATH) else None
-    )
-
-    if response.status_code == 200:
-        release_data = response.json()
-        data = [asset for asset in release_data["assets"] if asset["name"].endswith(".tar.gz")]
-        if len(data) > 0:
-            asset = data[0]
-            if file_mod_date is None or file_mod_date < datetime.datetime.strptime(
-                asset["created_at"], "%Y-%m-%dT%H:%M:%SZ"
-            ):
-                print("    Downloading dependency...")
-                url = asset["browser_download_url"]
-
-                response = requests.get(url, stream=True)
-                response.raise_for_status()
-
-                with open((RCCDC_PATH + ".tar.gz"), "wb") as f:
-                    for chunk in response.iter_content(chunk_size=8192):
-                        f.write(chunk)
-
-                print("    Dependency updated, extracting...")
-
-                os.chdir(os.path.dirname(RCCDC_PATH))
-                command = " ".join(["tar", "-xvzf", (RCCDC_PATH + ".tar.gz"), "-C", os.path.dirname(RCCDC_PATH)])
-                subprocess.run(
-                    command,
-                    check=True,
-                    shell=True,
-                    stdout=subprocess.DEVNULL,
-                    stderr=subprocess.PIPE,
+        shutil.copytree(
+            os.path.abspath(
+                os.path.join(
+                    os.path.dirname(__file__), "..", "submodules", "RCCDeckyCompanion", "out", "RCCDeckyCompanion"
                 )
-                os.unlink((RCCDC_PATH + ".tar.gz"))
-                print("    File extracted")
-            else:
-                print("    Dependency up to date")
-        else:
-            raise FileNotFoundError("No URL published for OpenRGB")
-    else:
-        raise Exception(f"Error getting latest release: {response.status_code}")
+            ),
+            RCCDC_PATH,
+        )
 
 
 print("Installing dependencies...")
