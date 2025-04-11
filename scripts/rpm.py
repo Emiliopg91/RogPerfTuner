467697b0429c54e76b2deef474fcc54c9ac86164
Emiliopg91 <ojosdeserbio@gmail.com>
2025-04-12 00:57:30 +0200

    Fix permissions

--- scripts/rpm.py
+++ scripts/rpm.py
@@ -40,6 +40,7 @@ def package_python():
     """Package Python application"""
     print("Packaging Python code...")
     shutil.copy2(os.path.join(workspace_dir, "pyproject.toml"), os.path.join(workspace_dir, "assets", "pyproject.toml"))
+    subprocess.run("chmod 777 -R .", shell=True, check=True)
     command = " ".join(
         [
             "pyinstaller",
@@ -126,7 +127,6 @@ install -m 755 """
 def generate_rpm():
     """Generate RPM package"""
     print("Generating RPM package...")
-    subprocess.run("chmod 777 -R .", shell=True, check=True)
     subprocess.run(
         f'rpmbuild --define "_topdir {rpm_dir}/rpmbuild" -ba {rpm_spec_file}', cwd=rpm_dir, shell=True, check=True
     )
