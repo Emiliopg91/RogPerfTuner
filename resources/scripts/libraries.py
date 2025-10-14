1155e3a0a41408a06596872e2f0d8a989e141681
Emiliopg91 <ojosdeserbio@gmail.com>
2025-10-14 16:11:40 +0200

    Integrate libraries in AppImage

--- resources/scripts/libraries.py
+++ resources/scripts/libraries.py
@@ -4,13 +4,12 @@ import subprocess
 
 os.makedirs("dist/appimage-fs/usr/lib",exist_ok=True)
 
-binaries = [ "assets/OpenRGB/OpenRGB","dist/RogPerfTuner/RogPerfTuner"]
+binaries = [ "build/assets/OpenRGB/OpenRGB","dist/RogPerfTuner/RogPerfTuner"]
 excluded = ["/lib64/ld-linux-x86-64.so.2", "linux-vdso.so.1", "libc.so.6", "libm.so.6", "libgcc_s.so.1"]
 
 for binary in binaries:
     print(f"Libaries for binary {binary}:")
     output=subprocess.run(f"ldd {binary} | sort", shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout
-    print(output)
 
     for line in output.splitlines():
         line=line.strip()
@@ -28,13 +27,7 @@ for binary in binaries:
         if libName not in excluded:
             shutil.copy2(libPath, "dist/appimage-fs/usr/lib")
 
-for e in os.listdir("dist/appimage-fs/usr/share/RogPerfTuner/OpenRGB/usr/lib"):
+for e in os.listdir("dist/appimage-fs/usr/share/rog-perf-tuner/OpenRGB/usr/lib"):
     if os.path.basename(e) != "udev":
-        os.unlink(f"dist/appimage-fs/usr/share/RogPerfTuner/OpenRGB/usr/lib/{e}")
-shutil.rmtree("dist/appimage-fs/usr/share/RogPerfTuner/OpenRGB/usr/plugins")
-
-plugin_folder = ["/usr/lib/qt6/plugins", "/usr/lib/x86_64-linux-gnu/qt6/plugins" ]
-for f in plugin_folder:
-    if os.path.exists(f):
-        shutil.copytree(f, "dist/appimage-fs/assets/usr/lib/qt6/plugins/")
-        break
+        os.unlink(f"dist/appimage-fs/usr/share/rog-perf-tuner/OpenRGB/usr/lib/{e}")
+shutil.rmtree("dist/appimage-fs/usr/share/rog-perf-tuner/OpenRGB/usr/plugins")
