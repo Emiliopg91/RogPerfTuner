31c34638de7d1004ba047a1d51a209d68a6971cb
Emiliopg91 <ojosdeserbio@gmail.com>
2025-10-30 15:46:15 +0100

    Count on me

--- RogPerfTuner/include/models/settings/application.hpp
+++ RogPerfTuner/include/models/settings/application.hpp
@@ -4,6 +4,7 @@
 
 struct Application {
 	bool askedInstallRccdc = false;
+	bool enroled		   = false;
 };
 
 // YAML-CPP serialization/deserialization
@@ -13,12 +14,16 @@ struct convert<Application> {
 	static Node encode(const Application& app) {
 		Node node;
 		node["askedInstallRccdc"] = app.askedInstallRccdc;
+		node["enroled"]			  = app.enroled;
 		return node;
 	}
 
 	static bool decode(const Node& node, Application& app) {
 		if (node["askedInstallRccdc"]) {
 			app.askedInstallRccdc = node["askedInstallRccdc"].as<bool>();
+		}
+		if (node["enroled"]) {
+			app.enroled = node["enroled"].as<bool>();
 		}
 		return true;
 	}
