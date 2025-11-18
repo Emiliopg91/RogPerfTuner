387537513c688397eee34c52bc1fe33612d4a9f8
Emiliopg91 <ojosdeserbio@gmail.com>
2025-11-18 22:40:38 +0100

    Changelog

--- RogPerfTuner/src/utils/constants.cpp
+++ RogPerfTuner/src/utils/constants.cpp
@@ -12,7 +12,7 @@ const pid_t Constants::PID = getpid();
 const std::string Constants::APP_NAME  = "RogPerfTuner";
 const std::string Constants::EXEC_NAME = "rog-perf-tuner";
 
-const std::string Constants::APP_VERSION = "4.8.7-1";
+const std::string Constants::APP_VERSION = "4.8.8-1";
 
 const std::string Constants::PLUGIN_VERSION = "1.2.7";
 
