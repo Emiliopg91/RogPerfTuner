c19f33c07813ba3325fed0fc0c529216ef2cc69d
Emiliopg91 <ojosdeserbio@gmail.com>
2026-03-04 12:05:57 +0100

    Workflow

--- resources/scripts/test.py
+++ resources/scripts/test.py
@@ -1,77 +1,91 @@
-with open("dist/PKGBUILD", "r", encoding="utf-8") as f:
-    pkgbuild_content = f.read()
-
-pkgbuild_content = pkgbuild_content.split("install=rog-perf-tuner")[0]
-pkgbuild_content = (
-    (
-        pkgbuild_content.split("depends=(")[1]
-        + "\n"
-        + pkgbuild_content.split("depends=(")[2]
-    )
-    .strip()
-    .replace("  ", "")
-).splitlines()
-
-content = []
-for line in pkgbuild_content:
-    if (
-        line == ")"
-        or line.strip() == "make"
-        or line.strip() == ")"
-        or len(line.strip()) == 0
-    ):
-        continue
-    content.append(line.replace("'", ""))
-
-
-dependencies = " ".join(content)
-
-dockerfile_content = f"""
-FROM archlinux:latest
-
-# Actualizar sistema e instalar dependencias base
-RUN pacman -Sy --noconfirm \
-    base-devel \
-    git \
-    sudo \
-    && pacman -Scc --noconfirm
-
-# Crear usuario no-root
-RUN useradd -m -G wheel -s /bin/bash builder \
-    && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-# Crear directorio donde se montarán los paquetes (como root)
-RUN mkdir -p /pkg && chmod 777 /pkg
-
-# Cambiar a usuario builder
-USER builder
-WORKDIR /home/builder
-
-# Instalar paru desde AUR
-RUN git clone https://aur.archlinux.org/paru.git \
-    && cd paru \
-    && makepkg -si --noconfirm \
-    && cd .. \
-    && rm -rf paru
-
-RUN paru -S --noconfirm {dependencies}
-
-WORKDIR /pkg
-
-# Ejecutar paru -U por defecto
-CMD ["bash", "-c", "paru -U --noconfirm --nocheck --noinstall && rm -rf /pkg/*"]
-"""
-
-with open("Dockerfile", "w", encoding="utf-8") as f:
-    f.write(dockerfile_content)
-
 import subprocess
-
-subprocess.run(["docker", "build", "-t", "installer-test", "."], check=True)
-
 import os
 
-os.unlink("Dockerfile")
+if os.getenv("GITHUB_ACTIONS", "false").lower() == "false":
+    with open("dist/PKGBUILD", "r", encoding="utf-8") as f:
+        pkgbuild_content = f.read()
+
+    pkgbuild_content = pkgbuild_content.split("install=rog-perf-tuner")[0]
+    pkgbuild_content = (
+        (
+            pkgbuild_content.split("depends=(")[1]
+            + "\n"
+            + pkgbuild_content.split("depends=(")[2]
+        )
+        .strip()
+        .replace("  ", "")
+    ).splitlines()
+
+    content = []
+    for line in pkgbuild_content:
+        if (
+            line == ")"
+            or line.strip() == "make"
+            or line.strip() == ")"
+            or len(line.strip()) == 0
+        ):
+            continue
+        content.append(line.replace("'", ""))
+
+    dependencies = " ".join(content)
+
+    dockerfile_content = f"""
+    FROM archlinux:latest
+
+    RUN pacman -Sy --noconfirm sudo \
+        && pacman -Scc --noconfirm
+    
+    # Crear usuario no-root
+    RUN useradd -m -G wheel -s /bin/bash builder \
+        && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+    # Crear directorio donde se montarán los paquetes (como root)
+    RUN mkdir -p /pkg && chmod 777 /pkg
+
+    # Cambiar a usuario builder
+    USER builder
+    WORKDIR /home/builder
+
+    # Instalar paru desde AUR
+    RUN sudo pacman -Sy --noconfirm base-devel git \
+        && git clone https://aur.archlinux.org/paru.git \
+        && cd paru \
+        && makepkg -si --noconfirm \
+        && cd .. \
+        && rm -rf paru \
+        && paru -S --noconfirm {dependencies} \
+        && sudo pacman -Scc --noconfirm
+
+    WORKDIR /pkg
+
+    # Ejecutar paru -U por defecto
+    CMD ["bash", "-c", "paru -U --noconfirm --nocheck --noinstall && sudo rm -rf /pkg/{{*,.*}} 2>/dev/null"]
+    """
+
+    with open("Dockerfile", "w", encoding="utf-8") as f:
+        f.write(dockerfile_content)
+
+    subprocess.run(
+        [
+            "docker",
+            "build",
+            "-t",
+            "epulidogil/rog-perf-tuner-installer-test:latest",
+            ".",
+        ],
+        check=True,
+    )
+
+    subprocess.run(
+        [
+            "docker",
+            "push",
+            "epulidogil/rog-perf-tuner-installer-test:latest",
+        ],
+        check=True,
+    )
+
+    os.unlink("Dockerfile")
 
 subprocess.run(
     [
@@ -80,7 +94,7 @@ subprocess.run(
         "-it",
         "-v",
         os.path.join(os.getcwd(), "dist:/pkg"),
-        "installer-test",
+        "epulidogil/rog-perf-tuner-installer-test:latest",
     ],
     check=True,
 )
